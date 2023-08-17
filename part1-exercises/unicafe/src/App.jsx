b97363a2b092c1d25d4098dd3fc0901e733f581d
Emilia Alanen <alanemi@digitalentshelsinki.internal>
2023-08-17 13:00:54 +0300

    1.8 done

--- part1-exercises/unicafe/src/App.jsx
+++ part1-exercises/unicafe/src/App.jsx
@@ -5,10 +5,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const all = good + neutral + bad
-  const average = (good * 1 + neutral * 0 + bad * -1) / all
-  const positive = good * 100 / all 
-
   const handleClick = (event) => {
     if (event.target.id === 'good') {
       setGood(count => count +1)
@@ -25,6 +21,23 @@ const App = () => {
       <Button text='good' handleClick={handleClick} id='good' />
       <Button text='neutral' handleClick={handleClick} id='neutral' />
       <Button text='bad' handleClick={handleClick} id='bad' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return <button onClick={props.handleClick} id={props.id}>{props.text}</button>
+}
+
+const Statistics = ({good, neutral, bad}) => {
+
+  const all = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * -1) / all
+  const positive = good * 100 / all 
+
+  return (
+    <div>
       <h2>Statistics</h2>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
@@ -34,10 +47,6 @@ const App = () => {
       <p>Positive: {positive ? positive : 'n/a'} {positive ? '%' : ''}</p>
     </div>
   )
-}
-
-const Button = (props) => {
-  return <button onClick={props.handleClick} id={props.id}>{props.text}</button>
 }
 
 export default App
