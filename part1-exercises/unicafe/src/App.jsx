8958529f24a87667fc774c4542abdd806ca9ad0c
Emilia Alanen <alanemi@digitalentshelsinki.internal>
2023-08-17 12:32:52 +0300

    1.7 done

--- part1-exercises/unicafe/src/App.jsx
+++ part1-exercises/unicafe/src/App.jsx
@@ -5,6 +5,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * -1) / all
+  const positive = good * 100 / all 
+
   const handleClick = (event) => {
     if (event.target.id === 'good') {
       setGood(count => count +1)
@@ -25,6 +29,9 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {average ? average : 'n/a'}</p>
+      <p>Positive: {positive ? positive : 'n/a'} {positive ? '%' : ''}</p>
     </div>
   )
 }
