4a2a1aae0da1145f275a5b3d72a4a79c09382c5b
Emilia Alanen <alanemi@digitalentshelsinki.internal>
2023-08-24 10:07:25 +0300

    2.6 done

--- part2-exercises/puhelinluettelo/src/App.jsx
+++ part2-exercises/puhelinluettelo/src/App.jsx
@@ -6,19 +6,35 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const newPerson = {name: newName}
+    setPersons(persons.concat(newPerson))
+    setNewName('') //resetting the input field
+  }
+
+  const personElements = persons.map(person => <li key={person.name}>{person.name}</li>)
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {personElements}
+      </ul>
+      
     </div>
   )
 
