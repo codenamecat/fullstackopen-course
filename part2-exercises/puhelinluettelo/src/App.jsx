7c85441be1f160dfcbf7a1c1b774a267f536cad1
Emilia Alanen <alanemi@digitalentshelsinki.internal>
2023-08-24 13:28:57 +0300

    2.9 done

--- part2-exercises/puhelinluettelo/src/App.jsx
+++ part2-exercises/puhelinluettelo/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Test Subject', number: '040-1111111'},
+    { name: 'Your Name Here', number: '00000'},
+    { name: 'This Guy', number: '12345'}
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -15,10 +19,16 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+  }
+
   const addName = (event) => {
     event.preventDefault()
+
     const newPerson = {name: newName, number: newNumber}
     const existingPerson = persons.find(person => person.name === newName)
+
     if (existingPerson) {
       alert(`${newName} is already added to the phonebook`)
     } else {
@@ -28,11 +38,15 @@ const App = () => {
     }
   }
 
-  const personElements = persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)
+  const personElements = persons.filter(person => person.name.toLowerCase().startsWith(search.toLowerCase())).map(person => <li key={person.name}>{person.name} {person.number}</li>)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        search: <input value={search} onChange={handleSearchChange} />
+      </div>
+      <h2>Add new person</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
