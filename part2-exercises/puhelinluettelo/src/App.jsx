74bf06b87d5224db381498ef79fe79faca86c69c
Emilia Alanen <alanemi@digitalentshelsinki.internal>
2023-09-05 11:53:50 +0300

    2.16 done

--- part2-exercises/puhelinluettelo/src/App.jsx
+++ part2-exercises/puhelinluettelo/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+  const [notifMessage, setNotifMessage] = useState('')
 
   useEffect(() => {
     peopleService
@@ -35,6 +36,12 @@ const App = () => {
 
     if (existingPerson) {
       peopleService.changePerson(existingPerson, newNumber)
+
+      setNotifMessage(`Changed the number for ${existingPerson.name}`)
+      setTimeout(() => {
+        setNotifMessage(null)
+      }, 4000)
+
       setNewName('')
       setNewNumber('')
     } else {
@@ -42,6 +49,12 @@ const App = () => {
         .createNew(newPerson)
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
+
+          setNotifMessage(`Added ${newPerson.name}`)
+          setTimeout(() => {
+            setNotifMessage(null)
+          }, 4000)
+
           setNewName('')
           setNewNumber('')
         })
@@ -51,11 +64,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notifMessage} />
       <SearchPerson search={search} handleChange={handleSearchChange} />
       <h2>Add new person</h2>
       <AddPerson newName={newName} newNumber={newNumber} handleSubmit={addName} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <NumberList persons={persons} search={search} />
+      <NumberList persons={persons} search={search} changeMessage={setNotifMessage} />
     </div>
   )
 }
@@ -84,15 +98,48 @@ const AddPerson = (props) => {
   )
 }
 
-const NumberList = ({ persons, search }) => {
+const NumberList = ({ persons, search, changeMessage }) => {
 
-  const personElements = persons.filter(person => person.name.toLowerCase().startsWith(search.toLowerCase())).map(person => <Number person={person} key={person.name} />)
+  const personElements = persons.filter(person => person.name.toLowerCase().startsWith(search.toLowerCase())).map(person => <Number person={person} key={person.name} changeMessage={changeMessage} />)
 
   return <ul>{personElements.length > 0 ? personElements : <div>No people found</div>}</ul>
 }
 
-const Number = ({ person }) => {
-  return <li>{person.name} {person.number} <button onClick={() => peopleService.deletePerson(person.id, person.name)}>Delete</button></li>
+const Number = ({ person, changeMessage }) => {
+
+  const deleteNumber = (id, name) => {
+    const confirmation = window.confirm(`Delete ${name}?`)
+
+    if (confirmation) {
+      peopleService.deletePerson(id)
+
+      changeMessage(`Deleted ${name}`)
+      setTimeout(() => {
+        changeMessage(null)
+      }, 4000)
+    }
+
+  }
+
+  return <li>{person.name} {person.number} <button onClick={() => deleteNumber(person.id, person.name)}>Delete</button></li>
+}
+
+const Notification = ({ message }) => {
+
+  const notificationStyle = {
+    backgroundColor: 'lightgreen',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginBottom: 10,
+  }
+
+  if (message === null) {
+    return null
+  } else {
+    return (
+      <div style={notificationStyle}>{message}</div>
+    )
+  }
 }
 
 export default App
--- part2-exercises/puhelinluettelo/src/services/people.js
+++ part2-exercises/puhelinluettelo/src/services/people.js
@@ -12,17 +12,14 @@ const createNew = newPerson => {
     return request.then(res => res.data)
 }
 
-const deletePerson = (idToDelete, nameToDelete) => {
-    const confirmation = window.confirm(`Delete ${nameToDelete}?`)
-    if (confirmation) {
-        axios.delete(`${baseUrl}/${idToDelete}`)
-    } 
+const deletePerson = (idToDelete) => {
+    axios.delete(`${baseUrl}/${idToDelete}`)
 }
 
 const changePerson = (personToChange, newNumber) => {
     const confirmation = window.confirm(`${personToChange.name} is already added to the phonebook, replace the old number with a new one?`)
     if (confirmation) {
-        const changedPerson = {...personToChange, number: newNumber}
+        const changedPerson = { ...personToChange, number: newNumber }
         axios.put(`${baseUrl}/${personToChange.id}`, changedPerson)
     }
 }
